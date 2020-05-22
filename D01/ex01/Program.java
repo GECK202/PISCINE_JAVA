864ca0b8c228d45f2c9963087d4c0a217b15e48b
vkaron <vkaron@student.21-school.ru>
2020-05-23 00:02:57 +0300

    add ex01

--- D01/ex01/Program.java
+++ D01/ex01/Program.java
@@ -10,9 +10,6 @@ public class Program {
 		if (user1.isSetUser("John", 777) && user2.isSetUser("Mike", 150)) {
 			System.out.println("name - " + user1.GetName() + " balance - " + user1.GetBalance() + " ID - " + user1.GetID());
 			System.out.println("name - " + user2.GetName() + " balance - " + user2.GetBalance() + " ID - " + user2.GetID());
-			if(tr.isSetTransaction(user1, user2, 50)) {
-				System.out.println(tr.GetPayee().GetName() + "->" + tr.GetSender().GetName() + ", " + tr.GetAmount() + ", " + tr.GetTranslation() + ", " + tr.GetID());
-			}
 		}
 	}
 
